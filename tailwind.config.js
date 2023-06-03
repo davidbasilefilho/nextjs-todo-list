2958c7882aa149d71d1b2be8fd69bbfbd7f3e26c
David Basile Filho <davidbasilefilho@gmail.com>
2023-06-03 18:24:24 -0300

    trying to test it on vercel, because it doesn't work locally

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,23 @@ module.exports = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
-    plugins: [],
+    theme: {
+        extend: {
+            colors: {
+                slate: {
+                    950: "#181926",
+                    900: "#1e2030",
+                    800: "#24273a",
+                    700: "#363a4f",
+                    600: "#494d64",
+                    500: "#5b6078",
+                    400: "#6e738d",
+                    300: "#939ab7",
+                    200: "#a5adcb",
+                    100: "#b8c0e0",
+                    50: "#cad3f5",
+                },
+            },
+        },
+    },
 };
